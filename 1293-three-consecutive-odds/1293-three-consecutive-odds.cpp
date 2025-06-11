bc838624dd83ed7b3b542723c43be7abbb828451
Adarsh Kumar <92328259+insanity-indeed@users.noreply.github.com>
2025-06-11 20:51:19 +0530

    Time: 0 ms (100%), Space: 12 MB (17.16%) - LeetHub

--- 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
@@ -3,7 +3,8 @@ public:
     bool threeConsecutiveOdds(vector<int>& num) {
         if(num.size() <= 2)  return 0;
         for(int i = 0 ; i < num.size() - 2 ; i++){
-            if(num[i] % 2 !=0 && num[i+1] % 2 !=0 && num[i+2] % 2 !=0){
+            int s = i ;
+            if(num[s] % 2 !=0 && num[s+1] % 2 !=0 && num[s+2] % 2 !=0){
                 return 1;
             }
         }
