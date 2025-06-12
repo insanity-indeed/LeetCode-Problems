96017dd382750bd3eaf6cdf21cec9e4a468ce049
Adarsh Kumar <92328259+insanity-indeed@users.noreply.github.com>
2025-06-12 18:59:30 +0530

    Time: 0 ms (100%), Space: 9.3 MB (49.9%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -2,38 +2,38 @@ class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         int rowStart = 0, rowEnd = matrix.size() - 1;
-    int colStart = 0, colEnd = matrix[0].size() - 1;
-    vector<int> ans;
+        int colStart = 0, colEnd = matrix[0].size() - 1;
+        vector<int> ans;
 
-    while (rowStart <= rowEnd && colStart <= colEnd) {
-        // Traverse Right
-        for (int i = colStart; i <= colEnd; i++) {
-            ans.push_back(matrix[rowStart][i]);
-        }
-        rowStart++;
-
-        // Traverse Down
-        for (int i = rowStart; i <= rowEnd; i++) {
-            ans.push_back(matrix[i][colEnd]);
-        }
-        colEnd--;
-
-        // Traverse Left
-        if (rowStart <= rowEnd) { // Check if there's a row left
-            for (int i = colEnd; i >= colStart; i--) {
-                ans.push_back(matrix[rowEnd][i]);
+        while (rowStart <= rowEnd && colStart <= colEnd) {
+            // Traverse Right
+            for (int i = colStart; i <= colEnd; i++) {
+                ans.push_back(matrix[rowStart][i]);
             }
-            rowEnd--;
-        }
+            rowStart++;
 
-        // Traverse Up
-        if (colStart <= colEnd) { // Check if there's a column left
-            for (int i = rowEnd; i >= rowStart; i--) {
-                ans.push_back(matrix[i][colStart]);
+            // Traverse Down
+            for (int i = rowStart; i <= rowEnd; i++) {
+                ans.push_back(matrix[i][colEnd]);
             }
-            colStart++;
+            colEnd--;
+
+            // Traverse Left
+            if (rowStart <= rowEnd) { // Check if there's a row left
+                for (int i = colEnd; i >= colStart; i--) {
+                    ans.push_back(matrix[rowEnd][i]);
+                }
+                rowEnd--;
+            }
+
+            // Traverse Up
+            if (colStart <= colEnd) { // Check if there's a column left
+                for (int i = rowEnd; i >= rowStart; i--) {
+                    ans.push_back(matrix[i][colStart]);
+                }
+                colStart++;
+            }
         }
-    }
-    return ans;
+        return ans;
     }
 };
