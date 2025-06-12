ba920ddbd8f89e92dba9ca9c62810eccecf97622
Adarsh Kumar <92328259+insanity-indeed@users.noreply.github.com>
2025-06-12 21:38:32 +0530

    Time: 0 ms (100%), Space: 23.4 MB (71.26%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n = nums.size();
-    
-        // BRUTE FORCE APPROACH
-        // Time Complexity: O(nlogn)
-        // Space Complexity: O(n)
-        vector<int> res;
+        /*
+        BRUTE FORCE APPROACH
+        Time Complexity: O(nlogn)
+        Space Complexity: O(n)
         map<int,int> mpp;
         int minOcc = n / 3 + 1 ;
         for(int i = 0 ; i < n ; i++){
@@ -15,7 +13,39 @@ public:
                 res.push_back(nums[i]);
             if(res.size()==2)   
                 break;
+        }*/
+        int n = nums.size();
+        vector<int> res;
+        int num1 = -1 , count1 = 0;
+        int num2 = -1 , count2 = 0;
+        for(int i = 0 ; i < n ; i ++){
+            if(count1 == 0 && num2 != nums[i]){
+                num1 = nums[i];
+                count1++;
+            }
+            else if(count2 == 0 && num1 != nums[i]){
+                num2 = nums[i];
+                count2++;
+            }
+            else if(nums[i] == num1){
+                count1++;
+            } 
+            else if(nums[i] == num2){
+                count2++;
+            }
+            else{
+                count1--;
+                count2--;
+            }
         }
+        int cnt1 = 0, cnt2 = 0;
+for (auto it : nums) {
+    if (it == num1) cnt1++;
+    else if (it == num2) cnt2++; // Only need to check remaining ones
+}
+
+if (cnt1 > n / 3) res.push_back(num1);
+if (cnt2 > n / 3) res.push_back(num2);
         return res;
     
     }
