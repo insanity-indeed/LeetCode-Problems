629e12f12248cdbd39308003c0e83ad2e22e853a
Adarsh Kumar <92328259+insanity-indeed@users.noreply.github.com>
2025-06-14 16:05:36 +0530

    Time: 9 ms (52.4%), Space: 44.9 MB (43.73%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -3,18 +3,17 @@ public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         int left = 0 ;
         int right = 0 ;
-        int ans = 0 ;
+
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());
 
         while(left<g.size() && right<s.size()){
             if(g[left]<=s[right]){
-                ans++;
                 left++;
             }
             right++;
         }
-        return ans;
+        return left;
 
     }
 };
