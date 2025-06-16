f4f7947fe73311e03bb332be657f9df4c84129fa
Adarsh Kumar <92328259+insanity-indeed@users.noreply.github.com>
2025-06-17 01:19:02 +0530

    Time: 266 ms (9.49%), Space: 17.8 MB (21.18%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -5,7 +5,7 @@ public:
        if(nums.size()<2)    return nums[0];
        for(int i = 0 ; i < nums.size() ; i++){
             if(nums[i] == 0)    continue;
-            double temp = 1 ;
+            double temp = 1   ;
             temp *= nums[i];
             ans = max(temp,ans);
             for(int j = i + 1 ; j < nums.size() ; j++){
