1e818f091b11cf07336ea2d4b1ce9941f2e7498b
Adarsh Kumar <92328259+insanity-indeed@users.noreply.github.com>
2025-06-26 20:54:03 +0530

    Time: 0 ms (100%), Space: 13.7 MB (5.7%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,17 +1,7 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size(); i++) {
-        if (nums[i] == target)
-            return i;  
-    }
-
-    for (int i = 0; i < nums.size(); i++) {
-        if (nums[i] < target && (i + 1 == nums.size() || nums[i + 1] > target)) {
-            return i + 1;
-        }
-    }
-
-    return 0;   
+       int lb = lower_bound(nums.begin(),nums.end(),target) - nums.begin() ;
+       return lb;      
     }
 };
