fb473fde61d5b0ab7a244915f6e4000a86042c6c
Adarsh Kumar <92328259+insanity-indeed@users.noreply.github.com>
2025-07-02 22:33:48 +0530

    Time: 94 ms (15.97%), Space: 18.8 MB (37.54%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -4,13 +4,19 @@ public:
         int rows = matrix.size() ;
         int cols = matrix[0].size() ;
         
-        for(int j = 0 ; j < cols ; j++){
+        /*for(int j = 0 ; j < cols ; j++){
             if(target >= matrix[0][j] && target <= matrix[rows-1][j]){
                 for(int i = 0 ; i < rows ; i++){
                     if(target == matrix[i][j])
                         return 1 ;
                 }
             }
+        }*/
+
+        for(int i = 0 ; i < rows ; i++){
+            int index = binary_search(matrix[i].begin(),matrix[i].end(),target);
+            if(index  )
+                return 1 ;
         }
         return 0 ;
 
