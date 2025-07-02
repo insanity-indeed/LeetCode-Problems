332a041ecf944ac5fd56c1fd253aba1b8f0dcd82
Adarsh Kumar <92328259+insanity-indeed@users.noreply.github.com>
2025-07-03 02:14:53 +0530

    Time: 23 ms (57.65%), Space: 17.5 MB (58.72%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>> v ;
         sort(nums.begin(),nums.end());
-
+        vector<vector<int>> v;
         for(int i = 0 ; i < nums.size() ; i++){
             if (i > 0 && nums[i] == nums[i - 1]) continue;
             for(int j = i + 1 ; j < nums.size() ; j++){
@@ -33,7 +32,6 @@ public:
                 }
             }
         }
-
-        return v;
+        return v;      
     }
 };
