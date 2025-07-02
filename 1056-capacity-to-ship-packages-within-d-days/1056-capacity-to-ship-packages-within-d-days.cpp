6b14908c3154b6ba929c14cd2a48ffa2492cd99e
Adarsh Kumar <92328259+insanity-indeed@users.noreply.github.com>
2025-07-03 00:52:11 +0530

    Time: 17 ms (18.64%), Space: 35.1 MB (34.36%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -23,11 +23,8 @@ public:
 
                 high = mid - 1 ; 
             }
-
-            if(day>days)
-                low = mid + 1;
             else
-                high = mid - 1 ;
+                low = mid + 1 ;
         }
         return low ;
     }
