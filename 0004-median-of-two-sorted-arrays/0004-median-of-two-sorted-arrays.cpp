1f31a22c5b7fe8319f9647a3d14b5f775fab1480
Adarsh Kumar <92328259+insanity-indeed@users.noreply.github.com>
2025-07-09 21:44:11 +0530

    Time: 0 ms (100%), Space: 95.1 MB (59.78%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int i = 0 , j = 0 ;
+        /*int i = 0 , j = 0 ;
         vector<int> res;
         while( i < nums1.size() && j < nums2.size() ){
             if(nums1[i] < nums2[j])
@@ -21,6 +21,40 @@ public:
             double ans = 0 ;
             ans = ((double)res[n/2] + (double)res[(n/2)-1])/2;
             return ans;
-        }   
+        }*/
+        int n = nums1.size() + nums2.size() ;
+        int ind1 = n / 2 ;
+        int ind2 = n / 2 - 1 ;
+        int element1 = -1 , element2 = -1 ;
+        int cnt = 0 ;
+        int i = 0 , j = 0 ;
+        while(i<nums1.size() && j<nums2.size()){
+            if(nums1[i] < nums2[j]){
+                if(ind1 == cnt) element1 = nums1[i];
+                if(ind2 == cnt) element2 = nums1[i]; 
+                i++;
+                cnt++;
+            }
+            else{
+                if(ind1 == cnt) element1 = nums2[j];
+                if(ind2 == cnt) element2 = nums2[j]; 
+                j++;
+                cnt++;
+            }
+        }
+        while(i<nums1.size()){
+            if(ind1 == cnt) element1 = nums1[i];
+            if(ind2 == cnt) element2 = nums1[i]; 
+            i++;
+            cnt++;
+        }  
+        while(j<nums2.size()){
+            if(ind1 == cnt) element1 = nums2[j];
+            if(ind2 == cnt) element2 = nums2[j]; 
+            j++;
+            cnt++;
+        }
+        if(n%2 != 0)    return element1;
+        else    return (double)(element1 + element2)/2; 
     }
 };
