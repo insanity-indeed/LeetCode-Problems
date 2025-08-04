8c1853285db243b2c1e02af4e5d8b2eb5df1e27c
Adarsh Kumar <92328259+insanity-indeed@users.noreply.github.com>
2025-08-04 21:40:45 +0530

    Time: 0 ms (100%), Space: 57.6 MB (19.12%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        map<int,int> mpp;
+        /*map<int,int> mpp;
         for(int i = 0 ; i < nums.size() ; i++){
             if(nums[i]>0)
                 mpp[nums[i]]++;
@@ -10,6 +10,20 @@ public:
             if(mpp.find(i) == mpp.end())
                 return i ;
         }
-        return nums.size() + 1;
+        return nums.size() + 1;*/
+        int n = nums.size();
+vector<int> freq(n + 1, 0);
+
+for (int i = 0; i < n; i++) {
+    if (nums[i] > 0 && nums[i] <= n)
+        freq[nums[i]] = 1;
+}
+
+for (int i = 1; i <= n; i++) {
+    if (freq[i] == 0)
+        return i;
+}
+
+return n + 1;
     }
 };
