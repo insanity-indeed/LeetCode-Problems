2124e638fbe81f9f1e69abdf9b903122fdcd0f07
Adarsh Kumar <92328259+insanity-indeed@users.noreply.github.com>
2025-08-07 19:30:15 +0530

    Time: 415 ms (7.4%), Space: 11.4 MB (84.85%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        int freq[26] = {0};
+        vector<int> ans;
+        int n = p.length();
+
+        // Safe check
+        if (s.length() < n)
+            return ans;
+
+        for (int i = 0; i < n; i++) {
+            freq[p[i] - 'a']++;
+        }
+
+        for (int i = 0; i <= s.length() - n; i++) {
+            int temp[26] = {0};
+            bool flag = true;
+
+            for (int j = i; j < i + n; j++) {
+                temp[s[j] - 'a']++;
+            }
+
+            for (int k = 0; k < 26; k++) {
+                if (temp[k] != freq[k]) {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (flag)
+                ans.push_back(i);
+        }
+
+        return ans;
+    }
+};
