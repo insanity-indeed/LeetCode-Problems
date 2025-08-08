0604bfab81acdf8ee1060fd329719f58ff9b23b7
Adarsh Kumar <92328259+insanity-indeed@users.noreply.github.com>
2025-08-08 18:44:30 +0530

    Time: 67 ms (12.3%), Space: 37.2 MB (5.71%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        int n = strs.size() ;
+        /*int n = strs.size() ;
         unordered_map<string , vector<string>> mpp; 
         vector<vector<string>> ans;
 
@@ -15,6 +15,27 @@ public:
             ans.push_back(it.second);
         }
 
-        return ans ;            
+        return ans ; */
+        int n = strs.size() ;
+        unordered_map<string , vector<string>> mpp ; 
+        vector<vector<string>> ans ;
+
+        for(int i = 0 ; i < n ; i++){
+            vector<int> temp(26,0);
+            for(auto c : strs[i])   temp[c-'a']++;
+
+            string str = "" ;
+            for (int j = 0; j < 26; j++) {
+        str += to_string(temp[j]) + "#"; // store counts
+    }
+
+            mpp[str].push_back(strs[i]);
+        }
+
+        for(auto it : mpp){
+            ans.push_back(it.second);
+        }
+
+        return ans ;           
     }
 };
