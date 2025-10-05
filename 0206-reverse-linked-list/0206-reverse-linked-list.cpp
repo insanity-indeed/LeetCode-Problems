ad2c26b20068ebe152d34cc58b35009b6d429039
Adarsh Kumar <92328259+insanity-indeed@users.noreply.github.com>
2025-10-05 11:52:57 +0530

    Time: 0 ms (100%), Space: 13.6 MB (18.03%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -10,47 +10,14 @@
  */
 class Solution {
 public:
-    ListNode* reverseLL(ListNode* temp , ListNode* prev){
-        if(temp == NULL)    return prev ;
-        ListNode* front = temp -> next ;
-        temp -> next = prev ;
-        prev = temp ;
-        temp = front;
-        return reverseLL(temp,prev);
-    }
     ListNode* reverseList(ListNode* head) {
-        /*if(head == NULL)    return head ;
-        vector<int> v ;
-        ListNode* temp = head ;
-        while(temp != NULL){
-            v.push_back(temp -> val);
-            temp = temp -> next ;
-        }
-        reverse(v.begin(),v.end());
-        ListNode* newHead = new ListNode(v[0]);
-        ListNode* newTemp = newHead ;
-        for(int i = 1 ; i < v.size() ; i++){
-            ListNode* newNode = new ListNode(v[i]);
-            newTemp-> next = newNode ;
-            newTemp = newTemp -> next ;
-        } 
-        return newHead ;
-        // Time Complexity -> O(3n)
-        Space Complexity -> O(n)
-        */
-        /*if(head == NULL)    return head ;
-        ListNode* temp = head ;
-        ListNode* prev = NULL ;
-        while(temp != NULL){
-            ListNode* front = temp -> next ;
-            temp -> next = prev ;
-            prev = temp ;
-            temp = front ;
-        }
-        return prev ;*/
-        if(head == NULL)    return head ;
-        ListNode* temp = head , *prev = NULL ;
-       return reverseLL(temp,prev);
-        //return prev;
+        if(head == NULL || head-> next == NULL)
+            return head ;
+        ListNode* newHead = reverseList(head->next) ;
+        ListNode* front = head-> next ;
+        front -> next = head ;
+        head -> next = NULL ;
+
+        return newHead;
     }
 };
