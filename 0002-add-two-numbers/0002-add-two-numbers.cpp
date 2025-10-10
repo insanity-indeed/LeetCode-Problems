09c43af0cba3d6874d70a587458955ccf6553713
Adarsh Kumar <92328259+insanity-indeed@users.noreply.github.com>
2025-10-10 19:22:39 +0530

    Time: 0 ms (100%), Space: 77.2 MB (14.05%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -10,51 +10,25 @@
  */
 class Solution {
 public:
-    ListNode* reverseList(ListNode* head){
-        if(head == NULL || head -> next == NULL)
-            return head ;
-        ListNode* newHead = reverseList(head -> next);
-        ListNode* front = head -> next ;
-        front -> next = head ;
-        head -> next = NULL ;
-        return newHead ;
-    }
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-       //l1 = reverseList(l1) ;
-       //l2 = reverseList(l2) ;
-       ListNode* t1 = l1 ;
-       ListNode* t2 = l2 ;
-       ListNode* dummy = new ListNode(-1);
-       ListNode* temp = dummy ;
-       int carry = 0 ;
-       while(t1 && t2){
-            int sum = t1 -> val + t2 -> val + carry ;
+        ListNode* t1 = l1 ;
+        ListNode* t2 = l2 ;
+        ListNode* dummy = new ListNode(-1);
+        ListNode* temp = dummy ;
+        int carry = 0 ;
+        while(t1 || t2){
+            int sum = carry ;
+            if(t1)  sum += t1 -> val ;
+            if(t2)  sum += t2 -> val ;
+
             temp -> next = new ListNode(sum % 10) ;
             carry = sum / 10 ;
             temp = temp -> next ;
-            t1 = t1 -> next ;
-            t2 = t2 -> next ;
-       }
-        if(t1){
-            while(t1){
-            int sum = t1 -> val + carry ;
-            temp -> next = new ListNode(sum % 10) ;
-            carry = sum / 10 ;
-            temp = temp -> next ;
-            t1 = t1 -> next ;
-            }
-        }
-        else{
-            while(t2){
-            int sum = t2 -> val + carry ;
-            temp -> next = new ListNode(sum % 10) ;
-            carry = sum / 10 ;
-            temp = temp -> next ;
-            t2 = t2 -> next ;
-            }
+            
+            if(t1)  t1 = t1 -> next ;
+            if(t2)  t2 = t2 -> next ;
         }
         dummy = dummy -> next ;
-        //dummy -> next = reverseList(dummy -> next);
         if(carry != 0){
             temp -> next = new ListNode(carry) ;
         }
