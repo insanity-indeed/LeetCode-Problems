825cc02d548ad0b93e63db97980275e13f53da86
Adarsh Kumar <92328259+insanity-indeed@users.noreply.github.com>
2025-10-23 16:06:45 +0530

    Time: 0 ms (100%), Space: 41.7 MB (39.26%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.java
+++ 0812-rotate-string/0812-rotate-string.java
@@ -1,13 +1,12 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if(s.equals(goal))   return true ;
         if(s.length() != goal.length()) return false ;
-
-        for(int i = 0 ; i < goal.length() ; i++){
-            goal = goal.substring(1) + goal.charAt(0);
-            if(goal.equals(s)) return true ;
-        }
-
-        return false ;
+        String temp = "" ;
+        temp += s ;
+        temp += s ;
+        if(temp.contains(goal))
+            return true ;
+        else 
+            return false ;
     }
 }
