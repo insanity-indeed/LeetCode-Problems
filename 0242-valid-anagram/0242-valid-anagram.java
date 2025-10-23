e251b46f87a4dec146a1511429389d927db58ade
Adarsh Kumar <92328259+insanity-indeed@users.noreply.github.com>
2025-10-23 16:15:14 +0530

    Time: 6 ms (41.63%), Space: 42.9 MB (89.37%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,10 +4,10 @@ class Solution {
         int[] cnt =  new int[26] ;
         for(int i = 0 ; i < s.length() ; i++){
             cnt[s.charAt(i) - 'a']++;
+            cnt[t.charAt(i) - 'a']--;
         }
-        for(int i = 0 ; i < t.length() ; i++){
-            if(cnt[t.charAt(i) - 'a'] == 0) return false ;
-            else cnt[t.charAt(i) - 'a']--;
+        for(int i = 0 ; i < 26 ; i++){
+            if(cnt[i] != 0) return false ;
         }
         return true ;
     }
