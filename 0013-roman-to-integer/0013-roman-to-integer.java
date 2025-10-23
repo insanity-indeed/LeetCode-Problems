b9cbe1629e9e19c7ad37688c8a06b516c1a60bbc
Adarsh Kumar <92328259+insanity-indeed@users.noreply.github.com>
2025-10-23 18:31:48 +0530

    Time: 3 ms (82.41%), Space: 44.9 MB (36.3%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,22 +1,26 @@
 class Solution {
+    private int value(char c){
+        switch(c){
+            case 'I' : return 1 ;
+            case 'V' : return 5 ;
+            case 'X' : return 10 ;
+            case 'L' : return 50 ;
+            case 'C' : return 100 ;
+            case 'D' : return 500 ;
+            case 'M' : return 1000 ;
+            default : return 0 ;
+        }
+    }
     public int romanToInt(String s) {
         HashMap<Character , Integer> roman = new HashMap<>() ;
-        roman.put('I',1);
-        roman.put('V',5);
-        roman.put('X',10);
-        roman.put('L',50);
-        roman.put('C',100);
-        roman.put('D',500);
-        roman.put('M',1000);
-
         int ans = 0 ;
         for(int i = 0 ; i < s.length() - 1 ; i++){
-            if(roman.get(s.charAt(i)) < roman.get(s.charAt(i+1)))
-                ans -= roman.get(s.charAt(i)) ;
+            if(value(s.charAt(i)) < value(s.charAt(i+1)))
+                ans -= value(s.charAt(i)) ;
             else
-                ans += roman.get(s.charAt(i)) ;
+                ans += value(s.charAt(i)) ;
         }
-        ans += roman.get(s.charAt(s.length() - 1));
+        ans += value(s.charAt(s.length() - 1));
         return ans ;
 
     }
