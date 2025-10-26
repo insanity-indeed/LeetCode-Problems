e43c3003e2d32b6ccc34c46e82220a3fbd0da84d
Adarsh Kumar <92328259+insanity-indeed@users.noreply.github.com>
2025-10-27 01:32:39 +0530

    Time: 5 ms (87.68%), Space: 44.6 MB (78.14%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,12 +1,12 @@
 class Solution {
-    public boolean isUnique(String sb){
-        int[] freq = new int[128] ;
-        for(int i = 0 ; i < sb.length() ; i++)  freq[sb.charAt(i)]++ ;
-        for(int i = 0 ; i < 128 ; i++){
-            if(freq[i] > 1) return false ;
-        }
-        return true ;
-    }
+    // public boolean isUnique(String sb){
+    //     int[] freq = new int[128] ;
+    //     for(int i = 0 ; i < sb.length() ; i++)  freq[sb.charAt(i)]++ ;
+    //     for(int i = 0 ; i < 128 ; i++){
+    //         if(freq[i] > 1) return false ;
+    //     }
+    //     return true ;
+    // }
     public int lengthOfLongestSubstring(String s) {
         if(s.length() == 0)    return 0 ;
         if(s.length() == 1)    return 1 ;
@@ -15,14 +15,14 @@ class Solution {
         int len = 0 ;
         int maxLen = 0 ;
 
-        StringBuilder sb = new StringBuilder() ;
-
+        //StringBuilder sb = new StringBuilder() ;
+        HashMap<Character, Integer> seen = new HashMap<>() ;
+        
         while(right < s.length()){
-            sb.append(s.charAt(right)) ;
-            if(!isUnique(sb.toString())){
-                sb.deleteCharAt(0) ;
-                left++ ;
+            if(seen.containsKey(s.charAt(right))){
+                left = Math.max(seen.get(s.charAt(right))+1,left) ;   
             }
+            seen.put(s.charAt(right) , right );
             maxLen = Math.max(maxLen , right - left + 1);
             right++ ;
         }   
