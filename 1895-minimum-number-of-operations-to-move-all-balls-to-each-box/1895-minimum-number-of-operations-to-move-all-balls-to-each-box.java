27506af04266b0fdbf2b816bab35004d5ae75cf8
Adarsh Kumar <92328259+insanity-indeed@users.noreply.github.com>
2025-10-26 11:43:34 +0530

    Time: 4 ms (76.14%), Space: 44.7 MB (74.66%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,21 +1,20 @@
 class Solution {
-    public int findOps(String s , int i){
-        int cnt = 0 ;
-        for(int j = 0 ; j < s.length() ; j++){
-            if(i == j)  continue ;
-            char c = s.charAt(j) ;
-            if(c == '1'){
-                cnt += Math.abs(i-j);
-            }
-        }
-        return cnt ;
-    }
     public int[] minOperations(String boxes) {
+        int balls = 0 ;
+        int moves = 0 ;
         int n = boxes.length() ;
         int[] res = new int[n] ;
         for(int i = 0 ; i < n ; i++){
-            res[i] = findOps(boxes , i) ;
+            res[i] += moves + balls ;
+            moves += balls ;
+            balls += boxes.charAt(i) - '0' ;
         }
+        balls = moves = 0 ;
+        for(int i = n - 1 ; i >= 0 ; i--){
+            res[i] += moves + balls ;
+            moves += balls ;
+            balls += boxes.charAt(i) - '0' ;
+        } 
         return res ;
     }
 }
