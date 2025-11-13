a3e1597977a2c09eadf927381d479330230367f9
Adarsh Kumar <92328259+insanity-indeed@users.noreply.github.com>
2025-11-13 23:33:53 +0530

    Time: 0 ms (100%), Space: 42.8 MB (6.84%) - LeetHub

---
+++ 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int cnt = 0 ;
+        int ans = 0 ;
+        for(int i = 0 ; i < s.length() ; i++){
+            if(s.charAt(i) == 'R')  cnt++ ;
+            else cnt-- ;
+            if(cnt == 0)    ans++ ;
+        }
+        return ans ;
+    }
+}
