515046f3b40e9a98cf02caca02905e44ca338a35
Adarsh Kumar <92328259+insanity-indeed@users.noreply.github.com>
2025-11-14 04:24:23 +0530

    Time: 0 ms (100%), Space: 7.9 MB (82.47%) - LeetHub

---
+++ 0953-reverse-only-letters/0953-reverse-only-letters.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int left = 0 ; 
+        int right = s.length() - 1 ;
+        while(left <= right){
+            if(!isalpha(s[left])){
+                left++ ;
+                continue;
+            }
+
+            if(!isalpha(s[right])){
+                right-- ;
+                continue;
+            }
+
+            swap(s[left++],s[right--]);
+    
+        }
+        return s ;
+    }
+};
