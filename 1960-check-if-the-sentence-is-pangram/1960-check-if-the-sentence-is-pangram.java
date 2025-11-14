30e6395bad77b15cdde208a988076fa4a748139d
Adarsh Kumar <92328259+insanity-indeed@users.noreply.github.com>
2025-11-14 19:57:39 +0530

    Time: 2 ms (57.04%), Space: 42.9 MB (7.16%) - LeetHub

---
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        int[] freq = new int[26] ;
+        for(int i = 0 ; i < sentence.length() ; i++){
+            char a = sentence.charAt(i) ;
+            freq[a-'a']++ ;
+        }
+
+        for(int i : freq){
+            if(i == 0)  return false ;
+        }
+
+        return true ;
+    }
+}
