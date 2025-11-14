eb14182c18fa314433bb5249f6eca6edfce62f33
Adarsh Kumar <92328259+insanity-indeed@users.noreply.github.com>
2025-11-14 19:48:21 +0530

    Time: 1 ms (17.1%), Space: 42.7 MB (11.73%) - LeetHub

--- 0742-to-lower-case/0742-to-lower-case.java
+++ 0742-to-lower-case/0742-to-lower-case.java
@@ -1,5 +1,12 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase() ;
+        StringBuilder ans = new StringBuilder() ;
+        for(int i = 0 ; i < s.length() ; i++){
+            char a = s.charAt(i);
+            if(a >= 'A' && a <= 'Z')
+                a += 'a' - 'A' ; 
+            ans.append(a) ;
+        }
+        return ans.toString() ;
     }
 }
