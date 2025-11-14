e72cedbd09aaba4148c2550a3bda8e49f839895a
Adarsh Kumar <92328259+insanity-indeed@users.noreply.github.com>
2025-11-15 03:33:55 +0530

    Time: 2 ms (35.95%), Space: 43.4 MB (21.1%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -5,29 +5,12 @@ class Solution {
         int j = b.length() - 1 ;
         int carry = 0 ;
         while(i >= 0 || j >= 0){
-            char first = i >= 0 ? a.charAt(i--) : '0' ;
-            char second = j >= 0 ? b.charAt(j--) : '0' ;
+            int sum = carry ;
+            if(i >= 0)  sum += a.charAt(i--) - '0' ;
+            if(j >= 0)  sum += b.charAt(j--) - '0' ;
 
-            if(first == '0' && second == '0'){
-                if(carry == 0)  str.append("0") ;
-                else    str.append("1") ;
-                carry = 0 ;
-            }
-            else if((first == '0' && second == '1') || (first == '1' && second == '0')){
-                if(carry == 0)  str.append("1") ;
-                else{
-                    str.append("0") ;
-                    carry = 1 ;
-                }    
-            }
-            else{
-                if(carry == 0){
-                    str.append("0");
-                }
-                else
-                    str.append("1") ;
-                carry = 1 ;
-            }
+            str.append(sum % 2) ;
+            carry = sum / 2 ;
         }
         if(carry == 1)  str.append("1");
         str.reverse() ;
