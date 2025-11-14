8cf0d5a090d200449d89d693ac8f50843f62e616
Adarsh Kumar <92328259+insanity-indeed@users.noreply.github.com>
2025-11-15 03:36:00 +0530

    Time: 1 ms (99.79%), Space: 43.5 MB (18.29%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -4,16 +4,17 @@ class Solution {
         int i = a.length() - 1 ;
         int j = b.length() - 1 ;
         int carry = 0 ;
-        while(i >= 0 || j >= 0){
+
+        while(i >= 0 || j >= 0 || carry > 0){
             int sum = carry ;
+            
             if(i >= 0)  sum += a.charAt(i--) - '0' ;
             if(j >= 0)  sum += b.charAt(j--) - '0' ;
 
             str.append(sum % 2) ;
             carry = sum / 2 ;
         }
-        if(carry == 1)  str.append("1");
-        str.reverse() ;
-        return str.toString() ;
+        
+        return str.reverse().toString() ;
     }
 }
