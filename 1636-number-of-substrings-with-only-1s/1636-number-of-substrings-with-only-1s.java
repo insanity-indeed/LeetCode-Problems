760ffc470ef0a89517792c3b043af01c9dc56969
Adarsh Kumar <92328259+insanity-indeed@users.noreply.github.com>
2025-11-17 20:45:59 +0530

    Time: 6 ms (23.26%), Space: 46.5 MB (6.15%) - LeetHub

--- 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.java
+++ 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.java
@@ -5,14 +5,14 @@ class Solution {
         long ans = 0 ;
         for(int i = 0 ; i < s.length() ; i++){
             char ch = s.charAt(i) ;
-            if(ch == '0'){
-                ans += (cnt * (cnt + 1)) / 2  % mod;
-                cnt = 0 ;
+            if(ch == '1'){
+                cnt++ ;
+                ans = (ans + cnt) % mod;
             }
             else 
-                cnt++ ;
+                cnt = 0 ;
         }
-        ans += (cnt * (cnt + 1)) / 2 ;
+       // ans += (cnt * (cnt + 1)) / 2 ;
         return (int)ans ;
     }
 }
