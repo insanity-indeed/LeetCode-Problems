3c8f714fff4d0d3021176dfb9864c409fc1fde0d
Adarsh Kumar <92328259+insanity-indeed@users.noreply.github.com>
2025-11-18 06:59:58 +0530

    Time: 2 ms (93.68%), Space: 46.4 MB (16.84%) - LeetHub

---
+++ 3540-hash-divided-string/3540-hash-divided-string.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String stringHash(String s, int k) {
+        int sum = 0 ;
+        int cnt = 0 ;
+        StringBuilder sb = new StringBuilder() ;
+        for(int i = 0 ; i < s.length() ; i++){
+            cnt++ ;
+            sum += s.charAt(i) - 'a' ; 
+            if(cnt == k){
+                char ch = 'a' ;
+                ch += (sum % 26) ;
+                sb.append(ch) ;
+                cnt = 0 ;
+                sum = 0 ;
+            }  
+        }   
+        return sb.toString() ;
+    }
+}
