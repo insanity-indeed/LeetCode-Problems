9ce8db3fb3b392cf9553faab8b89c00249e4c60a
Adarsh Kumar <92328259+insanity-indeed@users.noreply.github.com>
2025-11-18 23:19:28 +0530

    Time: 10 ms (43.98%), Space: 47.7 MB (6.89%) - LeetHub

---
+++ 2502-sort-the-people/2502-sort-the-people.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+        HashMap<Integer , String> mpp = new HashMap<>() ;
+        for(int i = 0 ; i < names.length ; i++){
+            mpp.put(heights[i] , names[i]);
+        }
+        Arrays.sort(heights);
+        //Arrays.sort(numbers, Collections.reverseOrder());
+        String[] ans = new String[names.length] ;
+        for(int i = names.length - 1 ; i >= 0  ; i--){
+            ans[names.length - 1 - i] = mpp.get(heights[i]) ;
+        }
+        return ans ;
+    }
+}
